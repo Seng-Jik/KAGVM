a46c8d2ca19c34f1799cd3b2eacb6a260406f1ce
许兴逸 <853974536@qq.com>
2022-05-22 17:37:51 +0800

    IDGenerator

---
+++ Utils.fs
@@ -0,0 +1,26 @@
+namespace Utils
+
+open System.Collections
+open System.Collections.Generic
+
+
+type IDGenerator<'a when 'a: comparison> () = 
+    let mutable generated = Map.empty: Map<'a, uint64>
+    let mutable nextId = 0UL
+    
+    member _.Item
+        with get index =
+            match Map.tryFind index generated with
+            | Some x -> x
+            | None ->
+                let curId = nextId
+                nextId <- nextId + 1UL
+                generated <- Map.add index curId generated
+                curId
+    
+    interface IEnumerable with
+        member _.GetEnumerator () = (Map.toSeq generated).GetEnumerator ()
+
+    interface IEnumerable<'a * uint64> with
+        member _.GetEnumerator () = (Map.toSeq generated).GetEnumerator ()
+
